8bb525c29f5e2a3e0372ad694a300cef10e9e1f9
Negar Pirasteh <negarpr@hotmail.com>
2024-10-12 20:36:46 -0400

    first commit

---
+++ map_of_game.py
@@ -0,0 +1,10 @@
+# Loop for repetition
+# Ask : roll the dice?
+# If user enter y
+#   Generate two random numbers
+#   Print them
+# If user enters n
+#   print thank you message
+#   Terminate Program
+# Else
+#   print Invalid choice
